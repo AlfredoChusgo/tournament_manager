4436ee59d3348dc5164251144fc6c349de55366c
AlfredoCh <alfredochsoliz@gmail.com>
2023-02-21 10:51:29 -0400

    added basic test

---
+++ src/__test__/App.test.js
@@ -0,0 +1,9 @@
+import { screen } from "@testing-library/react";
+import App from "../App";
+import { renderWithProviders } from "../utils/utils-for-tests";
+
+test("renders learn react link", () => {
+  renderWithProviders(<App />);
+  const linkElement = screen.getByText(/Welcome to the Home Page/i);
+  expect(linkElement).toBeInTheDocument();
+});
