ca206da3c43a481c3db7c0bdb659cde5c5247289
AlfredoChusgo <AlfredoChusgo@users.noreply.github.com>
2023-02-21 21:30:01 +0000

    deploy: 5a2cbe682621cd2231e653f84421fb9c25bbc1a8

--- src/__test__/App.test.js
+++
@@ -1,9 +0,0 @@
-import { screen } from "@testing-library/react";
-import App from "../App";
-import { renderWithProviders } from "../utils/utils-for-tests";
-
-test("renders learn react link", () => {
-  renderWithProviders(<App />);
-  const linkElement = screen.getByText(/Welcome to the Home Page/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/devnotes/objeto_etapa_gupo.js
+++
@@ -1,3 +0,0 @@
-// al agregar un objeto etapa grupo este 
-// muestra un modal con las sgt opciones 
-// cantidad de equipos por grupo
